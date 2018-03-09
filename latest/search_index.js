616cb29cbda919939b19d463741e947ef15643b4
autodocs <autodocs>
2018-03-09 10:04:08 +0000

    build based on 121e2f7

---
+++ latest/search_index.js
@@ -0,0 +1,51 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#BandedMatrices.jl-Documentation-1",
+    "page": "Home",
+    "title": "BandedMatrices.jl Documentation",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "index.html#Creating-block-banded-and-banded-block-banded-matrices-1",
+    "page": "Home",
+    "title": "Creating block-banded and banded-block-banded matrices",
+    "category": "section",
+    "text": "BlockBandedMatrixBandedBlockBandedMatrix"
+},
+
+{
+    "location": "index.html#BlockBandedMatrices.blockbandwidths",
+    "page": "Home",
+    "title": "BlockBandedMatrices.blockbandwidths",
+    "category": "function",
+    "text": "blockbandwidths(A)\n\nReturns a tuple containing the upper and lower blockbandwidth of A.\n\n\n\n"
+},
+
+{
+    "location": "index.html#BlockBandedMatrices.blockbandwidth",
+    "page": "Home",
+    "title": "BlockBandedMatrices.blockbandwidth",
+    "category": "function",
+    "text": "blockbandwidth(A,i)\n\nReturns the lower blockbandwidth (i==1) or the upper blockbandwidth (i==2).\n\n\n\n"
+},
+
+{
+    "location": "index.html#Accessing-block-banded-and-banded-block-banded-matrices-1",
+    "page": "Home",
+    "title": "Accessing block-banded and banded-block-banded matrices",
+    "category": "section",
+    "text": "blockbandwidthsblockbandwidthsubblockbandwidthssubblockbandwidth"
+},
+
+]}
