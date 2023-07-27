cb486f936b70dcae4413ac353991c53f8d8da9ef
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-07-27 10:10:04 +0530

    Don't check for Project.toml formatting on v1.6 (#162)

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,10 @@ using Test
 
 using Aqua
 @testset "Project quality" begin
-    Aqua.test_all(BlockBandedMatrices, ambiguities=false, piracy=false)
+    Aqua.test_all(BlockBandedMatrices, ambiguities=false, piracy=false,
+        # only test formatting on VERSION >= v1.7
+        # https://github.com/JuliaTesting/Aqua.jl/issues/105#issuecomment-1551405866
+        project_toml_formatting = VERSION >= v"1.9")
 end
 
 include("test_blockbanded.jl")
