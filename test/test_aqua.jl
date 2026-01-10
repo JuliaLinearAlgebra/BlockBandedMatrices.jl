baf04eb421a45c0c6020e6a3ddf18bb6789cf6c0
Jishnu Bhattacharya <jishnub.github@gmail.com>
2026-01-10 16:46:16 +0400

    Separate module for Aqua tests

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,4 +1,11 @@
+module AquaTests
+
+using BlockBandedMatrices
+using Test
+
 import Aqua
 @testset "Project quality" begin
-    Aqua.test_all(BlockBandedMatrices, ambiguities=false, piracies=false)
+    Aqua.test_all(BlockBandedMatrices, ambiguities=false, piracies=(; broken=true,))
+end
+
 end
